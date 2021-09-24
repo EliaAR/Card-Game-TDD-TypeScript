350bd802ac1a7b67b4f0c885974e8c1acd99123a
EliaAR <alonsoramos.elia@gmail.com>
2021-09-24 12:28:17 +0200

    TDD: implement test for character image

--- src/Components/Common/Character/Character.test.tsx
+++ src/Components/Common/Character/Character.test.tsx
@@ -1,9 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import { Character } from "./Character";
 
-describe("character exist", () => {
-  it("stats exist", () => {
-    render(<Character life={7} strength={8} dexterity={9} />);
+function Setup() {
+  return render(
+    <Character
+      srcImgCharacter="https://via.placeholder.com/150"
+      life={7}
+      strength={8}
+      dexterity={9}
+    />
+  );
+}
+
+describe("character is display corretly", () => {
+  it("image is shown", () => {
+    Setup();
+    const CharacterImg = screen.getByAltText(
+      /imagen de personaje/i
+    ) as HTMLImageElement;
+
+    expect(CharacterImg.src).toBe("https://via.placeholder.com/150");
+  });
+  it("stats are shown", () => {
+    Setup();
     const life = screen.getByText(/vida: 7/i);
     const strength = screen.getByText(/fuerza: 8/i);
     const dexterity = screen.getByText(/destreza: 9/i);
@@ -12,10 +31,4 @@ describe("character exist", () => {
     expect(strength).toBeInTheDocument();
     expect(dexterity).toBeInTheDocument();
   });
-  //   it("atack works", () => {
-  //     render(<Character />);
-  //   });
-  //   it("defens works", () => {
-  //     render(<Character />);
-  //   });
 });
