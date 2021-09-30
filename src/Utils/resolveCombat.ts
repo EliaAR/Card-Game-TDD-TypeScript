2a18865105a9816ab17f7e6f37dc1741087c359d
EliaAR <alonsoramos.elia@gmail.com>
2021-09-30 18:20:20 +0200

    TDD: refactor resolveCombat utils to pass test

--- src/Utils/resolveCombat.ts
+++ src/Utils/resolveCombat.ts
@@ -1,6 +1,18 @@
-function resolveCombat(strength: number, dexterity: number): number {
-  if (strength > dexterity) {
-    return strength - dexterity;
+import { rollD20, rollD4 } from "./dices";
+import { getAbilityModifier } from "./getAbilityModifier";
+
+function resolveCombat(
+  strength: number,
+  dexterity: number,
+  roll20 = rollD20,
+  roll4 = rollD4
+): number {
+  const strengthModifier = getAbilityModifier(strength);
+  const dexterityModifier = getAbilityModifier(dexterity);
+  const attackRoll = roll20() + strengthModifier;
+  const armorClass = 10 + dexterityModifier;
+  if (attackRoll >= armorClass) {
+    return roll4() + strengthModifier;
   } else {
     return 0;
   }
