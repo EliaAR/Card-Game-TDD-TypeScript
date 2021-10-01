8b6cdfe3649ac2523384fb86177578302e6aa00d
EliaAR <alonsoramos.elia@gmail.com>
2021-10-01 17:54:37 +0200

    TDD: fix CombatLog component to pass test

---
+++ src/Components/CombatLog/CombatLog.tsx
@@ -0,0 +1,21 @@
+interface CombatLogProps {
+  messages: string[];
+}
+
+function CombatLog({ messages }: CombatLogProps) {
+  return (
+    <section>
+      <p>Registro de combate</p>
+      {messages.length ? (
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No hay entradas en el registro</p>
+      )}
+    </section>
+  );
+}
+export { CombatLog };
