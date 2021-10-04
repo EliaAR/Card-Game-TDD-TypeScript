4170cfb50d01bcfc94b5b04fa0949d3629f1920f
EliaAR <alonsoramos.elia@gmail.com>
2021-10-04 20:50:32 +0200

    TDD: implement test for App component

--- src/Components/App/App.test.tsx
+++ src/Components/App/App.test.tsx
@@ -1,8 +1,50 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, getByText } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { App } from "./App";
 
-describe("", () => {
-  it("", () => {
-    render(<App />);
+function Setup(
+  mock20Enemy: () => number,
+  mock4Enemy: () => number,
+  mock20Player: () => number,
+  mock4Player: () => number
+) {
+  return render(
+    <App
+      roll20Enemy={mock20Enemy}
+      roll4Enemy={mock4Enemy}
+      roll20Player={mock20Player}
+      roll4Player={mock4Player}
+    />
+  );
+}
+
+describe("Combat ends correctly", () => {
+  it("Player wins", () => {
+    Setup(
+      () => 12,
+      () => 3,
+      () => 8,
+      () => 3
+    );
+    const enemyButton = screen.getByRole("button", { name: "pendiente" });
+    for (let index = 0; index < 6; index++) {
+      userEvent.click(enemyButton);
+    }
+    const winMsg = screen.getByText(/has ganado!!/i);
+    expect(winMsg).toBeInTheDocument();
+  });
+  it("Player lose", () => {
+    Setup(
+      () => 8,
+      () => 2,
+      () => 16,
+      () => 3
+    );
+    const enemyButton = screen.getByRole("button", { name: "pendiente" });
+    for (let index = 0; index < 10; index++) {
+      userEvent.click(enemyButton);
+    }
+    const loseMsg = screen.getByText(/has perdido 😢/i);
+    expect(loseMsg).toBeInTheDocument();
   });
 });
