8c216aea59249b428253665284a7efc693d00009
EliaAR <alonsoramos.elia@gmail.com>
2021-10-06 13:49:33 +0200

    use fonts in player component

--- src/Components/Player/Player.tsx
+++ src/Components/Player/Player.tsx
@@ -1,15 +1,16 @@
 import { Character } from "../Common/Types";
+import "./player.scss";
 
 interface PlayerProps extends Character {}
 
 function Player({ name, srcImg, life, strength, dexterity }: PlayerProps) {
   return (
-    <section data-testid="playerSection">
+    <section data-testid="playerSection" className="player">
       <article>
-        <p>{name}</p>
+        <p className="player__name">{name}</p>
         <img src={srcImg} alt={`Imagen ${name}`} />
       </article>
-      <article>
+      <article className="player__statsContainer">
         <p>Vida: {life} </p>
         <p>Fuerza: {strength} </p>
         <p>Destreza: {dexterity} </p>
