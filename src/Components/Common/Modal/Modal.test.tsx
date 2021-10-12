b5fc5a94d7dfeee9dc549a197f8e868605ad8609
EliaAR <alonsoramos.elia@gmail.com>
2021-10-12 16:26:12 +0200

    TDD: implement test for Modal component

---
+++ src/Components/Common/Modal/Modal.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Modal } from "./Modal";
+
+describe("modal works correctly", () => {
+  it("modal shows the children and a close button", () => {
+    const mockClose = jest.fn();
+    render(
+      <Modal onCloseModal={mockClose}>
+        <div>Relleno</div>
+      </Modal>
+    );
+    const divText = screen.getByText(/relleno/i);
+    expect(divText).toBeInTheDocument();
+    const buttonClose = screen.getByRole("button", { name: "Cerrar" });
+    userEvent.click(buttonClose);
+    const divOverlay = screen.getByTestId("modal-overlay");
+    userEvent.click(divOverlay);
+    expect(mockClose).toHaveBeenCalledTimes(2);
+  });
+});
