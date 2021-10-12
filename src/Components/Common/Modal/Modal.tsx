9ef082e84bd6c63f525668d590ea52529922bcca
EliaAR <alonsoramos.elia@gmail.com>
2021-10-12 18:12:29 +0200

    simplify Modal component

--- src/Components/Common/Modal/Modal.tsx
+++ src/Components/Common/Modal/Modal.tsx
@@ -12,24 +12,14 @@ modalRoot.setAttribute("id", "modal-root");
 document.body.appendChild(modalRoot);
 
 function Modal({ onCloseModal, children }: ModalProps) {
-  const element = document.createElement("div");
-
-  useEffect(() => {
-    modalRoot.appendChild(element);
-    return () => {
-      modalRoot.removeChild(element);
-    };
-  });
-
   return ReactDOM.createPortal(
-    <div onClick={onCloseModal} data-testid="modal-overlay">
-      <div onClick={(e) => e.stopPropagation()}>
+    <div onClick={onCloseModal} data-testid="modal-overlay" className="modal">
+      <div onClick={(e) => e.stopPropagation()} className="modal__container">
         {children}
-        <hr />
         <button onClick={onCloseModal}>Cerrar</button>
       </div>
     </div>,
-    element
+    modalRoot
   );
 }
 
