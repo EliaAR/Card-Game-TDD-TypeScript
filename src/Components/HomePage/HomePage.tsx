651749464476465a26fc9679663c83a1d7cb4d15
EliaAR <alonsoramos.elia@gmail.com>
2021-10-12 15:37:57 +0200

    add some design changes

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,22 +1,26 @@
+import "./HomePage.scss";
+
 interface HomePageProps {
   onClickStart: () => void;
 }
 
 function HomePage({ onClickStart }: HomePageProps) {
   return (
-    <>
-      <section>
-        <p>Truthy, The Card Game</p>
-        <p>Andar x Casa Productions®</p>
-        <p>
+    <main className="mainHomepage">
+      <section className="mainHomepage__text">
+        <p className="mainHomepage__gameTitle">Truthy, The Card Game</p>
+        <p className="mainHomepage__gameCompany">Andar x Casa Productions®</p>
+        <p className="mainHomepage__gameSubtitle">
           El juego de cartas donde podrás combatir con los verdaderos héroes que
           luchan contra sus opresores
         </p>
       </section>
       <section>
-        <button onClick={onClickStart}>Nueva partida</button>
+        <button onClick={onClickStart} className="mainHomepage__button">
+          Nueva partida
+        </button>
       </section>
-    </>
+    </main>
   );
 }
 
