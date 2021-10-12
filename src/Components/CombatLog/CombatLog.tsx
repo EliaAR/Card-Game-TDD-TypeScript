52cbdf218f969413541066dfc15a8bbde9748773
EliaAR <alonsoramos.elia@gmail.com>
2021-10-12 20:19:57 +0200

    add scroll into view in CombatLog component

--- src/Components/CombatLog/CombatLog.tsx
+++ src/Components/CombatLog/CombatLog.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { MessageObject } from "../Common/Types";
 import "./CombatLog.scss";
 
@@ -6,15 +7,23 @@ interface CombatLogProps {
 }
 
 function CombatLog({ messages }: CombatLogProps) {
+  const lastMsgRef = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    lastMsgRef.current?.scrollIntoView();
+    console.log(lastMsgRef.current);
+  });
+
   return (
     <section role="log" className="combatLog">
       <h3 className="combatLog__title">Registro de combate</h3>
       {messages.length ? (
         <ul>
-          {messages.map((message, index) => (
+          {messages.map((message, index, array) => (
             <li
               key={index}
               className={`combatLog__msg combatLog__msg--${message.type}`}
+              ref={array.length - 1 === index ? lastMsgRef : undefined}
             >
               {message.text}
             </li>
